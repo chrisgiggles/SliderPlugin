8c6c15bf6c741ce99c92528ab27dcea653756843
Christo Georgiev <christo@Christos-MacBook-Pro.local>
2015-02-22 10:24:20 +0100

    Added comments

--- scripts.js
+++ scripts.js
@@ -1,19 +1,26 @@
+/*==================================================================================================*/
+/* Slider Constructor
+/* Initializes the variables used throughout the script.
+/* Accepts three arguments, the container element, the navigation element and optionally the navigation 
+/* dots element.
+/*==================================================================================================*/
+
 function Slider( container, nav, dots ) {
 	this.container = container;
 	this.nav = nav;
 	this.dots = dots;
 
 	this.slides = this.container.find(".slide").children();
-	this.slideWidth = this.slides[0].width;
+	this.slideWidth = this.slides[0].width; //Sets the slider width based on the first slide
 	this.slideLength = this.slides.length;
 	this.current = 0;
 }
 
 Slider.prototype.init = function() {
-	//Update container width
+	//Set container width
 	this.container.width(this.slideWidth * this.slideLength);
 
-	//Generate nav dots if dots has been passed in
+	//Generate nav dots if dots has been passed in to the constructor
 	if (this.dots !== undefined) {
 		//Refactor to only update DOM once
 		for(var i = 0; i < this.slideLength; i++) {
@@ -31,16 +38,19 @@ Slider.prototype.init = function() {
 }
 
 Slider.prototype.setActiveDot = function( ) {
+	//Update the active class on the dots element
 	this.dots.children().removeClass("slidedot-active");
 	this.dots.children()[this.current].className = "slidedot-active";
 }
 
 Slider.prototype.setSlide = function( slideNum ) {
+	//Update variable
 	this.current = slideNum;
 	return this.current;
 }
 
 Slider.prototype.currentSlide = function( direction ) {
+	//Increment or decrement this.current
 	if( direction === "next" ) {
 		this.current++;
 	}
@@ -48,10 +58,11 @@ Slider.prototype.currentSlide = function( direction ) {
 		this.current--
 	}
 
-	if (this.current > this.slideLength - 1) {
+	//Check that this.current isn't below 0 or higher than the total number of slides
+	if ( this.current > this.slideLength - 1 ) {
 		this.current = 0;
 	} 
-	else if (this.current < 0) {
+	else if ( this.current < 0 ) {
 		this.current = this.slideLength - 1;
 	}
 
@@ -59,18 +70,20 @@ Slider.prototype.currentSlide = function( direction ) {
 }
 
 Slider.prototype.move = function( suffix ) {
+	//Use transform for better performance, should be pretty bulletproof for everything above IE8
 	this.container.css({ 
 		"-webkit-transform": "translate(" + (-(this.slideWidth * this.current)) + suffix + ")",
-		"transform": "translate(" + (-(this.slideWidth * this.current)) + suffix + ")",
+		   "-moz-transform": "translate(" + (-(this.slideWidth * this.current)) + suffix + ")",
+			"-ms-transform": "translate(" + (-(this.slideWidth * this.current)) + suffix + ")",
+				"transform": "translate(" + (-(this.slideWidth * this.current)) + suffix + ")",
 	});
 }
 
-/*=========================================================*/
+/*==================================================================================================*/
 /* Main function
-/* Uses a revealing module pattern
-/* Returns an init function, to kick off all scripts on the 
-/* page. Keeps all the configuration and private
-/*=========================================================*/
+/* Uses a revealing module pattern to keep all configuration private and keep the global scope clean
+/* Returns an init function, to kick off all scripts on the page.
+/*==================================================================================================*/
 
 var Main = (function() {
 
@@ -80,28 +93,26 @@ var Main = (function() {
 
 		$(".slide-nav").on("click", "a", function(e) {
 			e.preventDefault();
-			slider.currentSlide( $(this).data("dir") );
-			slider.setActiveDot();
+			slider.currentSlide( $(this).data("dir") ); //Use the data-dir attribute as argument
+			slider.setActiveDot(); 
 			slider.move("px");
 		});
 
 		$(".slide-dots").on("click", "a", function(e) {
 			e.preventDefault();
-			slider.setSlide( $(this).data("slide") );
+			slider.setSlide( $(this).data("slide") ); //Use the data-slide attribute as argument
 			slider.setActiveDot();
 			slider.move("px");
 		});
 	}
 
 	function init() {
-		slider();
+		slider(); //Run the function
 	}
 
 	return {
-		init: init
+		init: init //Return it
 	}
 })();
 
-Main.init();
-
-
+Main.init(); //GO!
